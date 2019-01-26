a61e50166b7cc0774357c655cc82c3e21a338de4
jamolpe <jamolpe@gmail.com>
2019-01-26 23:48:03 +0100

    Default URL status of the api

---
+++ Server/TiendeoAPI/TiendeoAPI/Controllers/ApiStatusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using TiendeoAPI.Models;
+
+namespace TiendeoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ApiStatusController : Controller
+    {
+        private readonly IOptions<Settings> _settings;
+
+        public ApiStatusController(IOptions<Settings> settings)
+        {
+            this._settings = settings;
+        }
+
+        [HttpGet]
+        public IActionResult Status()
+        {
+            return Ok(new {status = "Running", fakeDataMode= this._settings.Value.UseMockData});
+        }
+    }
+}
